a3b8c208d872187dd819e70fc2d40f7c7894412c
Facundo Gonzalez <facugon@interactar.com>
2018-04-03 18:31:49 -0300

    add trim function to remove first empty line

--- bin/ps-aux.js
+++ bin/ps-aux.js
@@ -9,7 +9,7 @@ var time = setInterval(() => {
       clearInterval(time)
       process.exit(1)
     }
-    console.log(res)
+    console.log(res[0], res[1])
     console.log('sleeping 2 seconds...')
   })
 }, 2000)
