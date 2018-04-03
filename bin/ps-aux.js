cf5d42a40a5ca1c2e2046f4681c71ef4fcff5f6e
Facugon <facugon@interactar.com>
2018-04-02 22:03:49 -0300

    lint using standard formatter

---
+++ bin/ps-aux.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var psaux = require('../')()
+
+var time = setInterval(() => {
+  psaux.parsed(function(err, res){
+    if (err) {
+      console.error(err)
+      clearInterval(time)
+      process.exit(1)
+    }
+    console.log(res)
+    console.log('sleeping 2 seconds...')
+  })
+}, 2000)
